1025d08b1f2a6c9ba1bb497bb1c097b77f9ee254
Sam Liao <sam.liao@viki.com>
2016-08-03 14:25:54 +0800

    [upgrade] Use latest rubymine

--- rubymine.sh
+++ rubymine.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 CMD="docker run \
-       --rm \
        --name rubymine \
        --net "host" \
        --env DISPLAY=unix$DISPLAY \
        --user 1000:1000 \
        --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-rubymine:latest"
+       dockerrubymine_ruby-mine"
 
 echo $CMD
 $CMD
